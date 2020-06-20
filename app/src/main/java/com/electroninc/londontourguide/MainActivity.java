a858e234f9f93cf2e4beec13a9bc7b600a11c0ab
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-20 21:13:13 +0100

    Colour changes

--- app/src/main/java/com/electroninc/londontourguide/MainActivity.java
+++ app/src/main/java/com/electroninc/londontourguide/MainActivity.java
@@ -1,11 +1,8 @@
 package com.electroninc.londontourguide;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
