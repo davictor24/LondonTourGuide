a0fd51e2165611a22396a1491f9299833c4c659b
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-21 12:05:50 +0100

    Implement PlacesViewModel

--- app/src/main/java/com/electroninc/londontourguide/lifecycle/PlacesViewModel.java
+++ app/src/main/java/com/electroninc/londontourguide/lifecycle/PlacesViewModel.java
@@ -1,4 +1,13 @@
 package com.electroninc.londontourguide.lifecycle;
 
-public class PlacesViewModel {
+import com.electroninc.londontourguide.models.Place;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PlacesViewModel extends ViewModel {
+    public MutableLiveData<List<Place>> places = new MutableLiveData<>((List<Place>) new ArrayList<Place>());
 }
