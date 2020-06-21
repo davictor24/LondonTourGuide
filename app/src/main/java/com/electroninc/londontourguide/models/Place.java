37437afaf6be1544954d6d4dbe25a59f1d013058
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-21 11:16:33 +0100

    Update Place model

--- app/src/main/java/com/electroninc/londontourguide/models/Place.java
+++ app/src/main/java/com/electroninc/londontourguide/models/Place.java
@@ -1,19 +1,51 @@
 package com.electroninc.londontourguide.models;
 
-public class Place {
-    private String title;
-    private String info;
+import java.util.List;
 
-    public Place(String title, String info) {
-        this.title = title;
+public class Place {
+    private String tag;
+    private String name;
+    private String info;
+    private String maps;
+    private String phone;
+    private String website;
+    private List<String> photos;
+
+    public Place(String tag, String name, String info, String maps, String phone, String website, List<String> photos) {
+        this.tag = tag;
+        this.name = name;
         this.info = info;
+        this.maps = maps;
+        this.phone = phone;
+        this.website = website;
+        this.photos = photos;
     }
 
-    public String getTitle() {
-        return title;
+    public String getTag() {
+        return tag;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getInfo() {
         return info;
+    }
+
+    public String getMaps() {
+        return maps;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
     }
 }
